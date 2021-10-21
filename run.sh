826336337ec99f47e3e9c8acb1b6d1e013af725c
Kodeing <casper.lindberg.petersson@gmail.com>
2021-10-21 19:03:22 +0200

    Updated run.sh since it didn't work

--- run.sh
+++ run.sh
@@ -1,35 +1,29 @@
-if command -v python
-then
-    if $(python -V 2>&1 | grep -Po '(?<=Python )(.)') == '3'
-    then
-        python ./square.py
-    else
-        echo "Python 3 is required"
+if command -v python then
+    if $(python -V 2>&1 | grep -Po '(?<=Python )(.)') == '3' then
+        python .square.py
+        exit
     fi
-elif command -v python3
-then
-    if $(python3 -V 2>&1 | grep -Po '(?<=Python )(.)') == '3'
-    then
-        python3 ./square.py
-    else
-        echo "Python 3 is required"
-    fi
-elif command -v py
-then
-    if $(py -V 2>&1 | grep -Po '(?<=Python )(.)') == '3'
-    then
-        py ./square.py
-    else
-        echo "Python 3 is required"
-    fi
-elif command -v py3
-then
-    if $(py3 -V 2>&1 | grep -Po '(?<=Python )(.)') == '3'
-    then
-        py3 ./square.py
-    else
-        echo "Python 3 is required"
-    fi
-else
-    echo "Could not find python, python3, py or py3, if you are using another alias run square.py with python 3"
 fi
+
+if command -v python3 then
+    if $(python3 -V 2>&1 | grep -Po '(?<=Python )(.)') == '3' then
+        python3 ./square.py
+        exit
+    fi
+fi
+
+if command -v py then
+    if $(py -V 2>&1 | grep -Po '(?<=Python )(.)') == '3' then
+        py ./square.py
+        exit
+    fi
+fi
+
+if command -v py3 then
+    if $(py3 -V 2>&1 | grep -Po '(?<=Python )(.)') == '3' then
+        py3 ./square.py
+        exit
+    fi
+fi
+
+echo "python, python3, py, py3 not found with a python 3 version"
