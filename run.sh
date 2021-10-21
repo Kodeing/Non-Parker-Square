d8a609a891eb048180cfa444bab5f87a6bc6ea9d
Kodeing <casper.lindberg.petersson@gmail.com>
2021-10-21 19:08:09 +0200

    Chanhed run.sh

--- run.sh
+++ run.sh
@@ -1,28 +1,28 @@
 if command -v python then
     if $(python -V 2>&1 | grep -Po '(?<=Python )(.)') == '3' then
         python .square.py
-        exit
+        exit()
     fi
 fi
 
 if command -v python3 then
     if $(python3 -V 2>&1 | grep -Po '(?<=Python )(.)') == '3' then
         python3 ./square.py
-        exit
+        exit()
     fi
 fi
 
 if command -v py then
     if $(py -V 2>&1 | grep -Po '(?<=Python )(.)') == '3' then
         py ./square.py
-        exit
+        exit()
     fi
 fi
 
 if command -v py3 then
     if $(py3 -V 2>&1 | grep -Po '(?<=Python )(.)') == '3' then
         py3 ./square.py
-        exit
+        exit()
     fi
 fi
 
